f1fe5c261161f60cc880b3a268cde4b543f2df41
clarabellelim <lim.qr.clarabelle@gmail.com>
2025-01-24 12:36:05 +0800

    Level 2. Add, List

--- src/main/java/Ghost.java
+++ src/main/java/Ghost.java
@@ -14,6 +14,10 @@ public class Ghost {
 
         String line = "____________________________________________________________";
 
+        // list of tasks
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("BOO! Hello I am\n" + logo + "\nHow may I haunt you?");
         System.out.println(line);
 
@@ -22,14 +26,35 @@ public class Ghost {
         while (true) {
             String input = scanner.nextLine();
 
+            // exit statement
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(line);
                 System.out.println(" Bye! I will always be haunting you...");
                 System.out.println(line);
                 break;
-            } else { 
+            }
+
+            // if user inputs "list", print list of tasks
+            if (input.equalsIgnoreCase("list")) {
                 System.out.println(line);
-                System.out.println(" " + input);
+                System.out.println(" BOO! Here's your list of things to HAUNT:\n");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("  " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+                continue;
+            }
+
+            //add new tasks 
+            if (taskCount < tasks.length) {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(line);
+                System.out.println(" New haunting item added: " + input);
+                System.out.println(line);
+            } else {
+                System.out.println(line);
+                System.out.println(" Error: The haunting list is full.");
                 System.out.println(line);
             }
         }
