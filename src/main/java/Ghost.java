82afd77a767d9e7788c079c1d8eb8550ea7b1fbb
clarabellelim <lim.qr.clarabelle@gmail.com>
2025-01-24 12:12:12 +0800

    Level 0

---
+++ src/main/java/Ghost.java
@@ -0,0 +1,20 @@
+public class Ghost {
+    public static void main(String[] args) {
+        String logo = "             ('-. .-.               .-')    .-') _    \n"
+                + "            ( OO )  /              ( OO ). (  OO) )   \n"
+                + "  ,----.    ,--. ,--. .-'),-----. (_)---\\_)/     '._  \n"
+                + " '  .-./-') |  | |  |( OO'  .-.  '/    _ | |'--...__) \n"
+                + " |  |_( OO )|   .|  |/   |  | |  |\\  :` `. '--.  .--' \n"
+                + " |  | .--, \\|       |\\_) |  |\\|  | '..`''.)   |  |    \n"
+                + "(|  | '. (_/|  .-.  |  \\ |  | |  |.-._)   \\   |  |    \n"
+                + " |  '--'  | |  | |  |   `'  '-'  '\\       /   |  |    \n"
+                + "  `------'  `--' `--'     `-----'  `-----'    `--'    \n";
+
+        String line = "____________________________________________________________";
+
+        System.out.println("BOO! Hello I am\n" + logo + "\nHow may I haunt you?");
+        System.out.println(line);
+        System.out.println("Bye! I will always be haunting you...");
+        System.out.println(line);
+    }
+}
