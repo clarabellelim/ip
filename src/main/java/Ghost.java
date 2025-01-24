c666a4ba47dda4e53e54283d1e416f2eff2c8fcb
clarabellelim <lim.qr.clarabelle@gmail.com>
2025-01-24 12:20:55 +0800

    Level 1. Echo

--- src/main/java/Ghost.java
+++ src/main/java/Ghost.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ghost {
     public static void main(String[] args) {
         String logo = "             ('-. .-.               .-')    .-') _    \n"
@@ -14,7 +16,25 @@ public class Ghost {
 
         System.out.println("BOO! Hello I am\n" + logo + "\nHow may I haunt you?");
         System.out.println(line);
-        System.out.println("Bye! I will always be haunting you...");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println(" Bye! I will always be haunting you...");
+                System.out.println(line);
+                break;
+            } else { 
+                System.out.println(line);
+                System.out.println(" " + input);
+                System.out.println(line);
+            }
+        }
+
+        scanner.close();
+
     }
 }
